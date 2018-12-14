66b1f2b621f08b1e586c44aadbce6585933e9907
Laksmack <junk1@kanon.net>
2018-12-14 10:11:56 -0600

    added starfall .....really?

--- balpal.lua
+++ balpal.lua
@@ -19,6 +19,7 @@ SB.DawningSun = 276152
 SB.Sunblaze = 274399
 SB.IncarnationBalance = 102560
 SB.FuryofElune = 202770
+SB.Starfall = 191034
 
 local outdoor = IsOutdoors()
 local indoor = IsIndoors()
